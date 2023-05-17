b01c7cf283f797ec69a7f5ebbf0f94faeb2fd0a8
Aphelele2 <aphelelejoyi@gmail.com>
2023-05-17 10:09:08 +0200

    Check

---
+++ script/code.js
@@ -0,0 +1,10 @@
+let sname = 'aphelele'
+console.log(typeof(sname));
+let age = 17
+console.log(typeof(age))
+let isMale = true
+console.log(typeof(isMale));
+let surname;
+console.log(typeof(surname));
+let height = null
+console.log(typeof(height));
